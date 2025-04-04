97e895f33900173e88d9cb5bfd83315156c246cc
bindok1 <akug53040@gmail.com>
2025-04-04 18:50:31 +0700

    fix:params-13

--- src/app/api/story-pages/[id]/route.ts
+++ src/app/api/story-pages/[id]/route.ts
@@ -6,7 +6,7 @@ const storyPageService = new StoryPageService();
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params:Promise<{ id: string }> }
 ) {
   try {
     const {id} = await params;
