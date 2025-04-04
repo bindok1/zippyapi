54dcc786959420c0151c34ca6173b179201af27c
bindok1 <akug53040@gmail.com>
2025-04-04 18:21:51 +0700

    fix:params-11

--- src/app/api/story-pages/[id]/route.ts
+++ src/app/api/story-pages/[id]/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import { StoryPageService } from '@/architecture/application/services/StoryPageService';
 
 const storyPageService = new StoryPageService();
-
+type Props = {
+  params: Promise<{
+    id: string
+  }>
+}
 
 
 export async function GET(
   request: NextRequest,
-  context: {id: string}
+  props: Props
 ) {
   try {
-    const {id} = await context;
+    const {id} = await props.params;
     const storyPage = await storyPageService.getStoryPageById(id);
     
     if (!storyPage) {
